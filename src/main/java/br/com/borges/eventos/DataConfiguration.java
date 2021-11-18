ea0d3ff62f51e1bf56c8bc4cbf946b732e50943d
f12borges <f12borges@gmail.com>
2021-11-18 14:33:46 -0300

    Criando e configurando um banco de dados MySQL

--- src/main/java/br/com/borges/eventos/DataConfiguration.java
+++ src/main/java/br/com/borges/eventos/DataConfiguration.java
@@ -1,39 +1,37 @@
 package br.com.borges.eventos;
 
-import javax.activation.DataSource;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@Profile("dev")
 public class DataConfiguration {
 
 	@Bean
-	public DataSource dataSource() {
-
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/evento");
-		dataSource.setUsername("root");
-		dataSource.setPassword("q1w2e3r4");
-		return (DataSource) dataSource;
-
-	}
-
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/evento");
+        dataSource.setUsername("root");
+        dataSource.setPassword("q1w2e3r4");
+        return dataSource;
+    }
+	
 	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
+	public JpaVendorAdapter jpaVendorAdapter(){
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
 		adapter.setPrepareConnection(true);
 		return adapter;
-
 	}
-
 }
