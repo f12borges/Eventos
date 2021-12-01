32d5b8c4fd11f6052ee95cd522bea471e3dd541f
f12borges <f12borges@gmail.com>
2021-12-01 07:57:13 -0300

    Relacionando entidades utilizando @OneToMany e @ManyToOne

--- src/main/java/br/com/borges/eventos/models/Evento.java
+++ src/main/java/br/com/borges/eventos/models/Evento.java
@@ -1,11 +1,13 @@
 package br.com.borges.eventos.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -30,6 +32,9 @@ public class Evento implements Serializable {
 	private String local;
 	private String data;
 	private String horario;
+	
+	@OneToMany
+	private List<Convidado> convidados;
 
 	public String getNome() {
 		return nome;
