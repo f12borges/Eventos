5929827c0bbfe535720e73c30e61991a702a8916
f12borges <f12borges@gmail.com>
2021-12-07 07:47:14 -0300

    Realizando buscas específicas no banco de dados

--- src/main/java/br/com/borges/eventos/repository/ConvidadoRepository.java
+++ src/main/java/br/com/borges/eventos/repository/ConvidadoRepository.java
@@ -3,7 +3,10 @@ package br.com.borges.eventos.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.borges.eventos.models.Convidado;
+import br.com.borges.eventos.models.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
-
+	
+	Iterable<Convidado> findByEvento(Evento evento);
+ 
 }
